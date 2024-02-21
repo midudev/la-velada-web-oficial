ca13cd8c7bb6edccbff8669a1a3bfb4b6bb61525
Miguel Ángel Durán <miduga@gmail.com>
2024-02-21 20:06:00 +0100

    🥊 VAMOS!!!!!

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+import animations from '@midudev/tailwind-animations'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'primary': '#333',
+				'secondary': '#ddd'
+			}
+		},
+	},
+	plugins: [animations],
+}
