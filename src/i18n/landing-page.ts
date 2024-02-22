0e36eb653f05f207efc80acbf9cb5e270fd52137
danmondra <danimonta15@gmail.com>
2024-02-22 02:36:18 -0600

    Add dictionary to spanish and english

---
+++ src/i18n/landing-page.ts
@@ -0,0 +1,58 @@
+export const LANDING_PAGE = {
+  es: {
+    'meta.description': 'Evento de boxeo entre streamers y creadores de contenido, organizado por Ibai Llanos',
+    'meta.title': "La Velada 4 - Evento de Boxeo de Ibai Llanos",
+    'meta.keywords': "velada, streamers, creadores, Ibai, boxeo, midudev",
+    'meta.og:image': '/og.webp',
+    'meta.og:site_name': "La Velada 4" ,
+    'meta.og:locale': 'es_ES',
+
+    'hero.presentation_of': 'Presentación de la',
+
+    'principal_date.presentation_event': 'Evento de Presentación',
+    'principal_date.victoria_theater': 'Teatro Victoria (Barcelona)',
+    'principal_date.available_tickets': 'Entradas agotadas',
+
+    'info.follow_it_in_stream': 'Síguelo en directo twitch.tv/ibai',
+    'info.presentation_interview_and_face_to_face': 'Presentación, Pesaje, Entrevista y Cara a Cara',
+    'info.with_presence_of_boxers': 'Con la presencia de los boxeadores y boxeadoras',
+
+    'countdown.days': 'Días',
+    'countdown.hours': 'Horas',
+    'countdown.minutes': 'Minutos',
+    'countdown.seconds': 'Segundos',
+
+    'footer.la_velada_of_year': '© 2024 La Velada del Año',
+    'footer.all_rights_reserved': 'Todos los derechos reservados.',
+    'footer.instagram_logo': 'Logo de Instagram',
+    'footer.x_logo': 'Logo de X, previamente conocido como Twitter'
+  },
+  en: {
+    'meta.description': 'Boxing event between streamers and content creators, hosted by Ibai Llanos',
+    'meta.title': "La Velada 4 - Boxing Event by Ibai Llanos",
+    'meta.keywords': "velada, streamers, creators, Ibai, boxing, midudev",
+    'meta.og:image': '/og.webp',
+    'meta.og:site_name': "La Velada 4" ,
+    'meta.og:locale': 'en_GB',
+
+    'hero.presentation_of': 'Presentation of the',
+
+    'principal_date.presentation_event': 'Presentation Event',
+    'principal_date.victoria_theater': 'Victoria Theater (Barcelona)',
+    'principal_date.available_tickets': 'Tickets sold out',
+
+    'info.follow_it_in_stream': 'Follow it live on twitch.tv/ibai',
+    'info.presentation_interview_and_face_to_face': 'Presentation, Weigh-In, Interview and Face-to-Face',
+    'info.with_presence_of_boxers': 'With the presence of both male and female boxers',
+
+    'countdown.days': 'Days',
+    'countdown.hours': 'Hours',
+    'countdown.minutes': 'Minutes',
+    'countdown.seconds': 'Seconds',
+
+    'footer.la_velada_of_year': '© 2024 La Velada del Año',
+    'footer.all_rights_reserved': 'All rights reserved.',
+    'footer.instagram_logo': 'Instagram Logo',
+    'footer.x_logo': 'X Logo, previously known as Twitter'
+  }
+} as const
