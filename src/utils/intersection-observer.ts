086c5e4cdafafeeccb064d83dcf62dd7a08ed798
Santiago Robra <santiagorobra@gmail.com>
2024-02-23 15:41:40 -0300

    add the invisible class to a constant

--- src/utils/intersection-observer.ts
+++ src/utils/intersection-observer.ts
@@ -1,16 +1,18 @@
+const INVISIBLE_TAILWIND_CLASS = 'invisible';
+
 export const setupIntersectionObserver = (elementId: string, animationClass: string, startInvisible: boolean) => {
   const element = document.querySelector(`#${elementId}`);
   if (!element) return;
 
   if (startInvisible) {
-    element.classList.add("invisible");
+    element.classList.add(INVISIBLE_TAILWIND_CLASS);
   }
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add(animationClass);
-        entry.target.classList.remove("invisible");
+        entry.target.classList.remove(INVISIBLE_TAILWIND_CLASS);
       }
     });
   }, { threshold: 0.2 });
