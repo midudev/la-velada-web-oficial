3c54f13fabad6bc4d9bd8f7bd6eaf461eff676a0
Santiago Robra <santiagorobra@gmail.com>
2024-02-23 11:49:57 -0300

    add flag for controlling initial visibility of a section

--- src/utils/intersection-observer.ts
+++ src/utils/intersection-observer.ts
@@ -1,11 +1,16 @@
-export const setupIntersectionObserver = (elementId: string, animationClass: string) => {
+export const setupIntersectionObserver = (elementId: string, animationClass: string, startInvisible: boolean) => {
   const element = document.querySelector(`#${elementId}`);
   if (!element) return;
+
+  if (startInvisible) {
+    element.classList.add("invisible");
+  }
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add(animationClass);
+        entry.target.classList.remove("invisible");
       }
     });
   }, { threshold: 0.2 });
