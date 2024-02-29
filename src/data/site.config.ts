6a0cd244b2fb001fad8b5e3add7597ccddcd7c6e
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2024-02-29 10:45:52 -0400

    fix: correccion al twitter de ibai, la velada y otros metadatos llevaods al site config para mejor manejo

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -1,8 +1,12 @@
 interface SiteConfig {
 	author: string
+	twitterSite: string
 	title: string
 	shortDescription: string
 	description: string
+	canonicalUrl: string
+	type: string
+	keywords: string
 	lang: string
 	ogLocale: string
 	shareMessage: string
@@ -10,10 +14,14 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: "@ibai", // Site author
+	author: "@IbaiLlanos", // Site author
+	twitterSite: "@infoLaVelada",
 	title: "La Velada del Año 4", // Site title.
 	shortDescription: "Evento de Boxeo de Ibai Llanos",
 	description: "Web Oficial de La Velada del Año IV, evento de boxeo entre streamers y creadores de contenido, organizado por Ibai Llanos", // Description to display in the meta tags
+	canonicalUrl: "https://lavelada.es",
+	type: "website",
+	keywords: "velada, streamers, creadores, Ibai, boxeo, midudev",
 	lang: "es-ES",
 	ogLocale: "es_ES",
 	shareMessage: "Share this post", // Message to share a post on social media
