f15e83a960cb6664ed00d3d94580bffe25ce3c10
Miguel Ángel Durán <miduga@gmail.com>
2024-03-05 19:21:48 +0100

    Agregada fuente "Atomic" y colores de acento y Twitch a tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,9 +5,13 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
+			fontFamily: {
+				atomic: ["Atomic", "cursive"],
+			},
 			colors: {
 				primary: "var(--color-primary)",
 				secondary: "var(--color-secondary)",
+				accent: "var(--color-accent)",
 				twitch: "var(--color-twitch)",
 				ice: "var(--color-twitch-ice)",
 			},
