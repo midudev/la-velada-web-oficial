0fd1dce5b124186862b8af9dc5f00b520c7e8fcd
Miguel Ángel Durán <miduga@gmail.com>
2024-03-06 11:02:33 +0100

    Extract sponsors to const

---
+++ src/consts/sponsors.ts
@@ -0,0 +1,47 @@
+export const SPONSORS = [
+	{
+		id: "vicio",
+		name: "Vicio",
+		url: "https://www.ganasdevicio.com/",
+	},
+	{
+		id: "revolut",
+		name: "Revolut",
+		url: "https://revolut.com/",
+	},
+	{
+		id: "el-pozo",
+		name: "El Pozo",
+		url: "https://elpozo.com/",
+	},
+	{
+		id: "grefusa",
+		name: "Grefusa",
+		url: "https://grefusa.com/",
+	},
+	{
+		id: "spotify",
+		name: "Spotify",
+		url: "https://spotify.com/",
+	},
+	{
+		id: "cerave",
+		name: "Cerave",
+		url: "https://cerave.es/",
+	},
+	{
+		id: "prime",
+		name: "Prime",
+		url: "https://drinkprime.com/",
+	},
+	{
+		id: "alsa",
+		name: "Alsa",
+		url: "https://alsa.es/",
+	},
+	{
+		id: "maxibon",
+		name: "Maxibon",
+		url: "https://maxibon.es/",
+	},
+] as const
