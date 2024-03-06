97d0ef135a3ca11f41ca34909519d62a68946648
Edier Gama <ediersantiago7@gmail.com>
2024-03-05 22:35:47 -0500

    fix: :art: Cambio a la estructura de los botones, modularizado y escalable en un solo componente
    En lugar de llamar a varios componentes como CalendarButton, se llama solo a ButtonLink y este abstrae la lógica entera y opcional dependiendo el id

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,9 @@
 import tailwind from "@astrojs/tailwind"
-import vercel from "@astrojs/vercel/serverless"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	adapter: vercel({
-		webAnalytics: { enabled: true },
-	}),
 	output: "hybrid",
 	vite: {
 		build: {
