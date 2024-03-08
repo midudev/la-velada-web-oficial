ab76463ff6d9377f2131c8749f47b89c31f0b536
John Serrano <andrey_201015@hotmail.com>
2024-03-08 15:51:55 -0500

    Add test home page and config playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { defineConfig, devices } from "@playwright/test"
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const URL = import.meta.env?.PUBLIC_URL || "http://localhost:4321"
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -24,7 +21,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: process.env.URL,
+		baseURL: URL,
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,43 @@
 import { expect, test } from "@playwright/test"
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
 	await page.goto("/")
-
-	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle(/13 DE JULIO/)
 })
 
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
+test.describe("Load home page", () => {
+	test("has title home page", async ({ page }) => {
+		await expect(page).toHaveTitle(/La Velada del Año 4 - Evento de Boxeo de Ibai Llanos/)
+	})
 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
+	test("has the text of the ibai channel", async ({ page }) => {
+		const label = "Enlace al canal de Twitch de"
+		await expect(page.getByLabel(label)).toBeVisible()
+		await expect(page.getByLabel(label)).toHaveText("twitch.tv/ibai")
+	})
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
+	test("has the text of the event date", async ({ page }) => {
+		const role = "time"
+		await expect(page.getByRole(role)).toBeVisible()
+		await expect(page.getByRole(role)).toHaveText("13 de julio", { timeout: 1000 })
+	})
+
+	test("has the text of the event location", async ({ page }) => {
+		const label = "ubicación del campo Santiago"
+		await expect(page.getByLabel(label)).toBeVisible()
+		await expect(page.getByLabel(label)).toHaveText(" Estadio Santiago Bernabéu ")
+	})
+
+	test("load section vídeo", async ({ page }) => {
+		const title = "Play Video"
+		await expect(page.getByTitle(title)).toBeVisible()
+	})
+
+	test("load the sponsors section and their logos", async ({ page }) => {
+		const text = "Patrocinadores La Velada"
+		await expect(page.getByText(text)).toBeVisible()
+
+		await expect(page.locator(".sponsors")).toBeVisible()
+		await expect(page.locator(".sponsors > div a")).toHaveCount(10)
+		await expect(page.getByRole("link", { name: "Logo del patrocinador Vicio" })).toBeVisible()
+	})
+})
