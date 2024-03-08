a9d9a84a0c744288b8f7c48481175c56a3b6d49e
JJozef <Josemillaquen12@gmail.com>
2024-03-07 22:08:48 -0300

    Add Konami function and added new konami codes

---
+++ src/consts/konami-codes.ts
@@ -0,0 +1,17 @@
+export const konamiCodes = {
+	rotateY: [
+		"ArrowUp",
+		"ArrowUp",
+		"ArrowDown",
+		"ArrowDown",
+		"ArrowLeft",
+		"ArrowRight",
+		"ArrowLeft",
+		"ArrowRight",
+		"b",
+		"a",
+	],
+	gloveCursor: ["g", "l", "o", "v", "e"],
+	amongUsCursor: ["a", "m", "o", "n", "g", "u", "s"],
+	lolCursor: ["l", "o", "l"],
+}
