d3a10721c2f3e20558dd94db6814120998660e98
AlejandroSuero <asuerome@gmail.com>
2024-03-09 04:28:45 +0100

    fix(#366): Added implicit sponsors images' width and height

--- src/consts/sponsors.ts
+++ src/consts/sponsors.ts
@@ -1,47 +1,93 @@
-export const SPONSORS = [
+interface Sponsors {
+	id: string
+	name: string
+	url: string
+	image: {
+		width: number | string
+		height: number | string
+	}
+}
+
+export const SPONSORS: Sponsors[] = [
 	{
 		id: "vicio",
 		name: "Vicio",
 		url: "https://www.ganasdevicio.com/",
+		image: {
+			width: 128,
+			height: 70,
+		},
 	},
 	{
 		id: "revolut",
 		name: "Revolut",
 		url: "https://revolut.com/",
+		image: {
+			width: 128,
+			height: 70,
+		},
 	},
 	{
 		id: "prime",
 		name: "Prime",
 		url: "https://drinkprime.com/",
+		image: {
+			width: 128,
+			height: 70,
+		},
 	},
 	{
 		id: "alsa",
 		name: "Alsa",
 		url: "https://alsa.es/",
+		image: {
+			width: 128,
+			height: 70,
+		},
 	},
 	{
 		id: "spotify",
 		name: "Spotify",
 		url: "https://spotify.com/",
+		image: {
+			width: 128,
+			height: 70,
+		},
 	},
 	{
 		id: "cerave",
 		name: "Cerave",
 		url: "https://cerave.es/",
+		image: {
+			width: 128,
+			height: 70,
+		},
 	},
 	{
 		id: "el-pozo",
 		name: "El Pozo",
 		url: "https://elpozo.com/",
+		image: {
+			width: 128,
+			height: 70,
+		},
 	},
 	{
 		id: "grefusa",
 		name: "Grefusa",
 		url: "https://grefusa.com/",
+		image: {
+			width: 128,
+			height: 70,
+		},
 	},
 	{
 		id: "maxibon",
 		name: "Maxibon",
 		url: "https://maxibon.es/",
+		image: {
+			width: 128,
+			height: 70,
+		},
 	},
 ] as const
