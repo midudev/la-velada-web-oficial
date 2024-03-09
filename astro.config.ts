14daeb03598db04a80f3b03e4af190fde263e5e0
jamerrq <jamerrq@gmail.com>
2024-03-09 01:03:54 -0500

    feat: pwa support

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,16 @@ import tailwind from "@astrojs/tailwind"
 import vercel from "@astrojs/vercel/serverless"
 import { defineConfig } from "astro/config"
 
+import sitemap from "@astrojs/sitemap"
+import compress from "astro-compress"
+import { VitePWA } from "vite-plugin-pwa"
+
+// Helper imports
+import { manifest, seoConfig } from "./src/utils/seoConfig"
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [tailwind(), sitemap(), compress()],
 	adapter: vercel({
 		webAnalytics: { enabled: true },
 	}),
@@ -12,6 +19,7 @@ export default defineConfig({
 		inlineStylesheets: "always",
 	},
 	output: "hybrid",
+	site: seoConfig.baseURL,
 	vite: {
 		build: {
 			cssMinify: "lightningcss",
@@ -19,5 +27,20 @@ export default defineConfig({
 		ssr: {
 			noExternal: ["path-to-regexp"],
 		},
+		plugins: [
+			VitePWA({
+				registerType: "autoUpdate",
+				manifest,
+				workbox: {
+					globDirectory: "dist",
+					globPatterns: [
+						"**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}"
+					],
+					// Don't fallback on document based (e.g. `/some-page`) requests
+					// This removes an errant console.log message from showing up.
+					navigateFallback: null
+				}
+			})
+		]
 	},
 })
