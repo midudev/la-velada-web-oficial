f93f49d3a17dda7ae45eac0ea6af82dca55210f6
John Serrano <andrey_201015@hotmail.com>
2024-03-11 12:01:39 -0500

    Resolvign conflict

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -6,13 +6,14 @@ test.beforeEach(async ({ page }) => {
 
 test.describe("Load home page", () => {
 	test("has title home page", async ({ page }) => {
-		await expect(page).toHaveTitle(/La Velada del Año 4 - Evento de Boxeo de Ibai Llanos/)
+		await expect(page).toHaveTitle("La Velada del Año 4 - Evento de Boxeo de Ibai Llanos")
 	})
 
 	test("has the text of the ibai channel", async ({ page }) => {
-		const label = "Enlace al canal de Twitch de"
-		await expect(page.getByLabel(label)).toBeVisible()
-		await expect(page.getByLabel(label)).toHaveText("twitch.tv/ibai")
+		const role = "link"
+		await expect(page.getByRole(role, { name: "Enlace al canal de Twitch de" })).toBeVisible()
+
+		await expect(page.getByText("en directo twitch.tv/ibai")).toBeVisible()
 	})
 
 	test("has the text of the event date", async ({ page }) => {
@@ -28,16 +29,17 @@ test.describe("Load home page", () => {
 	})
 
 	test("load section vídeo", async ({ page }) => {
-		const title = "Play Video"
-		await expect(page.getByTitle(title)).toBeVisible()
+		const text = "Presentación Vuelve a ver la"
+		await expect(page.getByText(text)).toBeVisible()
 	})
 
 	test("load the sponsors section and their logos", async ({ page }) => {
 		const text = "Patrocinadores La Velada"
+		const label = "Link con el logo de Vicio que"
 		await expect(page.getByText(text)).toBeVisible()
 
 		await expect(page.locator(".sponsors")).toBeVisible()
 		await expect(page.locator(".sponsors > div a")).toHaveCount(10)
-		await expect(page.getByRole("link", { name: "Logo del patrocinador Vicio" })).toBeVisible()
+		await expect(page.getByLabel(label)).toBeVisible()
 	})
 })
