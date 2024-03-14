8b5c816a34cf2b0ad73ef041f5649e3f20530d98
Joel Martínez <joel.mh112@gmail.com>
2024-03-14 18:28:33 +0100

    [FEAT] api route works

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly API_URL: string
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
+}
--- src/pages/api/combats/get-combat-by-boxer-id.json.ts
+++ src/pages/api/combats/get-combat-by-boxer-id.json.ts
@@ -1,9 +1,10 @@
 import { COMBATS } from "@/consts/combats"
 import type { APIRoute } from "astro"
+export const prerender = false
 
-export const GET: APIRoute = ({ params, request }) => {
-	const { boxerId } = params
-	console.log(boxerId)
+export const GET: APIRoute = ({ url }) => {
+	const boxerId = url.searchParams.get("boxerId")
+
 	const combat = COMBATS.find((combat) => combat.boxers.includes(boxerId as string))
 	if (combat) {
 		return new Response(JSON.stringify(combat), {
@@ -12,6 +13,6 @@ export const GET: APIRoute = ({ params, request }) => {
 			},
 		})
 	} else {
-		return new Response("Not found", { status: 404 })
+		return new Response(JSON.stringify("Not found"), { status: 404 })
 	}
 }
