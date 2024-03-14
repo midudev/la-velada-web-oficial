87a7dc60b936e9e7ad20dfeb041eafd49c3d64af
Joel Martínez <joel.mh112@gmail.com>
2024-03-15 00:39:36 +0100

    [FEAT] forecast section done
    faltaria ver a ver que hacemos rey de pista y 2vs2

--- src/consts/forecasts.ts
+++ src/consts/forecasts.ts
@@ -11,6 +11,51 @@ export interface ForecastData {
 
 export const FORECASTS: Forecast[] = [
 	{
+		combatId: "1",
+		forecastData: [
+			{
+				boxerId: "agustin-51",
+				forecast: 0.3,
+				predictionsCount: 30,
+			},
+			{
+				boxerId: "carreraaa",
+				forecast: 0.7,
+				predictionsCount: 70,
+			},
+		],
+	},
+	{
+		combatId: "2",
+		forecastData: [
+			{
+				boxerId: "guanyar",
+				forecast: 0.8,
+				predictionsCount: 80,
+			},
+			{
+				boxerId: "la-cobra",
+				forecast: 0.2,
+				predictionsCount: 20,
+			},
+		],
+	},
+	{
+		combatId: "4",
+		forecastData: [
+			{
+				boxerId: "shelao",
+				forecast: 0.9,
+				predictionsCount: 90,
+			},
+			{
+				boxerId: "viruzz",
+				forecast: 0.1,
+				predictionsCount: 10,
+			},
+		],
+	},
+	{
 		combatId: "6",
 		forecastData: [
 			{
