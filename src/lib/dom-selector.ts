cf82b2425cfe50f108324ff5830a3f232d534360
Miguel Ángel Durán <miduga@gmail.com>
2024-03-18 10:49:47 +0100

    Normalize usage of dom selector

--- src/lib/dom-selector.ts
+++ src/lib/dom-selector.ts
@@ -4,15 +4,19 @@
  * 	const elementClass = $('.my-class')
  * 	const elementId = $('#my-id')
  * @param selector
+ * @param context
  * @returns  HTMLElement
  */
-export const $ = (selector: string) => document.querySelector(selector) as HTMLElement
+export const $ = (selector: string, context: Document | HTMLElement = document) =>
+	context.querySelector(selector) as HTMLElement
 
 /**
  * Get elements from dom by selector string
  * @example
  * 	const elements = $$('.my-class')
  * @param selector
+ * @param context
  * @returns  NodeList
  */
-export const $$ = (selector: string) => document.querySelectorAll(selector)
+export const $$ = (selector: string, context: Document | HTMLElement = document) =>
+	context.querySelectorAll(selector)
