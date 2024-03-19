cc92b0a60217dc0ee50024468f0c9bddc06b218f
Miguel Ángel Durán <miduga@gmail.com>
2024-03-19 19:39:27 +0100

    Add new id for combats

--- src/consts/combats.ts
+++ src/consts/combats.ts
@@ -1,27 +1,33 @@
 export interface Combat {
 	id: string
 	boxers: string[] // es un string porque hace referencia a los ids de los boxeadores
+	number: number
 }
 
 export const COMBATS: Combat[] = [
 	{
-		id: "1",
+		id: "1-agustin-51-vs-carreraaa",
+		number: 1,
 		boxers: ["agustin-51", "carreraaa"],
 	},
 	{
-		id: "2",
+		id: "2-guanyar-vs-la-cobra",
+		number: 2,
 		boxers: ["guanyar", "la-cobra"],
 	},
 	{
-		id: "3",
+		id: "3-zeling-y-nissaxter-vs-alana-y-ama-blitz",
+		number: 3,
 		boxers: ["zeling", "alana", "nissaxter", "ama-blitz"],
 	},
 	{
-		id: "4",
+		id: "4-viruzz-vs-shelao",
+		number: 4,
 		boxers: ["viruzz", "shelao"],
 	},
 	{
-		id: "5",
+		id: "5-rey-de-la-pista",
+		number: 5,
 		boxers: [
 			"roberto-cein",
 			"aldo-geo",
@@ -36,7 +42,8 @@ export const COMBATS: Combat[] = [
 		],
 	},
 	{
-		id: "6",
+		id: "6-el-mariana-vs-plex",
+		number: 6,
 		boxers: ["el-mariana", "plex"],
 	},
 ]
