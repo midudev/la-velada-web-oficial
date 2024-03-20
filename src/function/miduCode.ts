23b5e7a59dfd9893b38426b96fe5eac362e30342
Darío Espinoza <dario@fixu.cl>
2024-03-20 19:12:27 -0300

    feat: :sparkles: Optimization Ant  for konami midu code

--- src/function/miduCode.ts
+++ src/function/miduCode.ts
@@ -4,8 +4,39 @@ interface Contributor {
 }
 
 export async function showContributors($miduContainer: HTMLDivElement) {
-	const contributors = await getContributors()
+	async function loadContributors() {
+		const contributorsLocalStorage = localStorage.getItem("contributors")
+		const contributors: Contributor[] = contributorsLocalStorage
+			? JSON.parse(contributorsLocalStorage)
+			: await getContributors()
 
+		// maps for the contributors for save types Contributors
+		const contributorsMap = contributors.map((contributor: Contributor) => {
+			return {
+				avatar_url: contributor.avatar_url,
+				login: contributor.login,
+				/* add more data here */
+			}
+		})
+		contributorsLocalStorage ||
+			localStorage.setItem("contributors", JSON.stringify(contributorsMap))
+		return await contributors
+	}
+	function convertImgUrlToBase64(name: string, url: string) {
+		const img = new Image()
+		img.src = url
+		img.crossOrigin = "Anonymous"
+		img.onload = function () {
+			const canvas = document.createElement("canvas")
+			const ctx = canvas.getContext("2d")
+			canvas.width = img.width
+			canvas.height = img.height
+			ctx?.drawImage(img, 0, 0)
+			const dataURL = canvas.toDataURL("image/png")
+			localStorage.setItem(name, dataURL)
+		}
+	}
+	const contributors = await loadContributors()
 	for (let i = 0; i < contributors.length; i++) {
 		setTimeout(() => {
 			const { avatar_url, login } = contributors[i]
@@ -14,13 +45,22 @@ export async function showContributors($miduContainer: HTMLDivElement) {
 			img.title = login
 			img.classList.add("bubbles")
 			if (login === "midudev") {
+				const localStorageItem = localStorage.getItem(login)
 				img.setAttribute("id", "midu")
-				img.src = `${avatar_url}&size=150`
+				img.src = localStorageItem
+					? localStorageItem || `${avatar_url}&size=150`
+					: `${avatar_url}&size=60`
+				// convert img url to base64 and save in localstorage
+				convertImgUrlToBase64(login, img.src)
 			} else {
-				img.src = `${avatar_url}&size=60`
+				const localStorageItem = localStorage.getItem(login)
+				img.src = localStorageItem
+					? localStorageItem || `${avatar_url}&size=60`
+					: `${avatar_url}&size=60`
 				img.style.left = `${generateRandomNumber()}vw`
 				const startRotation = Math.floor(Math.random() * (90 - -90 + 1)) + -45
 				img.style.transform = `rotate(${startRotation}deg)`
+				convertImgUrlToBase64(login, img.src)
 			}
 			img.addEventListener("animationend", () => {
 				$miduContainer.removeChild(img)
