b54ac9753f6b7a77e84f2d0240d83e603ac9ca2e
victorinoLavida <victorinolavida@ciencias.unam.mx>
2024-03-26 13:30:27 -0600

    feat: add user id in session

--- auth.config.js
+++ auth.config.js
@@ -6,6 +6,31 @@ export default defineConfig({
 		Twitch({
 			clientId: import.meta.env.TWITCH_CLIENT_ID,
 			clientSecret: import.meta.env.TWITCH_CLIENT_SECRET,
+			profile(profile) {
+				return {
+					id: profile.sub,
+					name: profile.preferred_username,
+					email: profile.email,
+					image: profile.picture,
+				}
+			},
+
 		}),
 	],
+	callbacks: {
+		async session({ token, session }) {
+			if (token) {
+				session.user.id = token.id
+			}
+			return session
+		},
+		async jwt({ token, user }) {
+			if (user) {
+				token.id = user.id
+				delete token.sub
+			}
+			return token
+		}
+	}
+
 })
