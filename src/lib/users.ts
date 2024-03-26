54e80c7526e6f8a18d704f098923056edbdccc27
Fabián Olivares Ríos <f.olivaresrios@gmail.com>
2024-03-26 14:13:37 -0600

    Remove unused file

--- src/lib/users.ts
+++
@@ -1,8 +0,0 @@
-import { createHash } from "node:crypto"
-
-import type { User } from "@auth/core/types"
-
-export const generateUserId = (user: User) => {
-	if (!user.email) throw new Error("User email is required")
-	return createHash("sha256").update(user.email).digest("hex")
-}
