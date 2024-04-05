45a63abd85b9157efd805fbb0e6328160304cc91
Miguel Ángel Durán <miduga@gmail.com>
2024-04-05 18:08:00 +0200

    Rollback middleware changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,27 +6,19 @@ import { Redis } from "@upstash/redis"
 
 import type { MiddlewareHandler } from "astro"
 
-const isProd = import.meta.env.VERCEL as string
+const redis = new Redis({
+	url: import.meta.env.UPSTASH_REDIS_REST_URL as string,
+	token: import.meta.env.UPSTASH_REDIS_REST_TOKEN as string,
+})
 
-let redis: Redis
-let ratelimit: Ratelimit
+const ratelimit = new Ratelimit({
+	redis,
+	limiter: Ratelimit.slidingWindow(10, "10 s"),
+	analytics: true,
+})
 
-if (isProd) {
-	redis = new Redis({
-		url: import.meta.env.UPSTASH_REDIS_REST_URL as string,
-		token: import.meta.env.UPSTASH_REDIS_REST_TOKEN as string,
-	})
-
-	ratelimit = new Ratelimit({
-		redis,
-		limiter: Ratelimit.slidingWindow(10, "10 s"),
-		analytics: true,
-	})
-}
-
-let checkAllowed: (request: Request) => Promise<boolean> = () => Promise.resolve(true) // eslint-disable-line prefer-const
-
-const checkRequest: string = isProd ? (import.meta.env.CHECK_REQUEST as string) : ""
+let checkAllowed: (request: Request) => Promise<boolean>
+const checkRequest: string = import.meta.env.CHECK_REQUEST as string
 const code = Buffer.from(checkRequest, "base64").toString("utf-8")
 eval(code) // eslint-disable-line no-eval
 
@@ -40,14 +32,8 @@ export const onRequest: MiddlewareHandler = async ({ request }, next) => {
 		request.headers.get("cf-connecting-ip") ??
 		"0.0.0.0"
 
-	if (ratelimit) {
-		const { success } = await ratelimit.limit(ip)
-		if (success) return await next()
-	}
-
-	console.log({ ratelimit, allowed })
-
-	if (allowed) return await next()
+	const { success } = await ratelimit.limit(ip)
+	if (success) return await next()
 
 	return new Response("", { status: 418 })
 }
