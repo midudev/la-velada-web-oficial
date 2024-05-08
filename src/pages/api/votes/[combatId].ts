5aa481f947f6cdaca9182c2cc9e7781fe60a83ad
AlejandroSuero <asuerome@gmail.com>
2024-05-08 22:23:49 +0200

    fix(#951): applying team match vote

--- src/pages/api/votes/[combatId].ts
+++ src/pages/api/votes/[combatId].ts
@@ -34,7 +34,12 @@ export const POST: APIRoute = async ({ params, request }) => {
 	if (!success) return res("Bad request", { status: 400 })
 
 	const { voteId } = output
-	const boxerData = combatData.boxers.find((b) => b === voteId)
+	const boxerData = combatData.boxers.find((b) => {
+		if (combatData.teams !== undefined) {
+			return voteId.includes(b)
+		}
+		return b === voteId
+	})
 	if (!boxerData) return res("Boxer not found", { status: 404 })
 
 	const userId = session.user.id
