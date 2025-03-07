cfcb851583b8c77d119fd9466c394ab0c5db7416
midudev <miduga@gmail.com>
2025-03-07 10:26:56 +0100

    Import sponsors svg

--- src/consts/sponsors.ts
+++ src/consts/sponsors.ts
@@ -1,9 +1,25 @@
 import type { Sponsors } from "@/types/Sponsors"
 
+import Alsa from "@/assets/sponsors/Alsa.svg"
+import Cerave from "@/assets/sponsors/Cerave.svg"
+import CocaCola from "@/assets/sponsors/CocaCola.svg"
+import ElPozo from "@/assets/sponsors/ElPozo.svg"
+import Grefusa from "@/assets/sponsors/Grefusa.svg"
+import GShock from "@/assets/sponsors/GShock.svg"
+import InfoJobs from "@/assets/sponsors/Infojobs.svg"
+import Mahou from "@/assets/sponsors/Mahou.svg"
+import Maxibon from "@/assets/sponsors/Maxibon.svg"
+import Nothing from "@/assets/sponsors/Nothing.svg"
+import Prime from "@/assets/sponsors/Prime.svg"
+import Revolut from "@/assets/sponsors/Revolut.svg"
+import Spotify from "@/assets/sponsors/Spotify.svg"
+import Vicio from "@/assets/sponsors/Vicio.svg"
+
 export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "alsa",
 		name: "Alsa",
+		icon: Alsa,
 		url: "https://alsa.es/",
 		image: {
 			width: 133 * 0.7 * 0.65,
@@ -13,6 +29,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "spotify",
 		name: "Spotify",
+		icon: Spotify,
 		url: "https://spotify.com/",
 		image: {
 			name: "spotify-3",
@@ -23,6 +40,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "revolut",
 		name: "Revolut",
+		icon: Revolut,
 		url: "https://revolut.com/",
 		image: {
 			width: 177 * 0.75 * 0.65,
@@ -32,6 +50,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "vicio",
 		name: "Vicio",
+		icon: Vicio,
 		url: "https://www.ganasdevicio.com/",
 		image: {
 			width: 164 * 0.6 * 0.65,
@@ -41,6 +60,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "coca-cola",
 		name: "Coca-Cola",
+		icon: CocaCola,
 		url: "https://www.coca-cola.com/es/es",
 		image: {
 			width: 220 * 0.4 * 0.65,
@@ -50,6 +70,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "infojobs",
 		name: "InfoJobs",
+		icon: InfoJobs,
 		url: "https://infojobs.net",
 		image: {
 			width: 140 * 0.65 * 0.65,
@@ -60,6 +81,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "grefusa",
 		name: "Grefusa",
+		icon: Grefusa,
 		url: "https://grefusa.com/",
 		image: {
 			width: 144 * 0.55 * 0.65,
@@ -69,6 +91,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "nothing",
 		name: "Nothing",
+		icon: Nothing,
 		url: "https://intl.nothing.tech/",
 		image: {
 			width: 144 * 0.7 * 0.65,
@@ -78,6 +101,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "cerave",
 		name: "Cerave",
+		icon: Cerave,
 		url: "https://cerave.es/",
 		image: {
 			width: 180 * 0.55 * 0.65,
@@ -87,6 +111,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "mahou",
 		name: "Mahou",
+		icon: Mahou,
 		url: "https://www.mahou.es/",
 		image: {
 			width: 200 * 0.35 * 0.65,
@@ -96,6 +121,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "maxibon",
 		name: "Maxibon",
+		icon: Maxibon,
 		url: "https://maxibon.es/",
 		image: {
 			width: 176 * 0.45 * 0.65,
@@ -105,6 +131,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "prime-h",
 		name: "Prime",
+		icon: Prime,
 		url: "https://drinkprime.com/",
 		image: {
 			width: 200 * 0.65,
@@ -114,6 +141,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "el-pozo",
 		name: "ElPozo",
+		icon: ElPozo,
 		url: "https://elpozo.com/",
 		image: {
 			width: 134 * 0.65,
@@ -123,6 +151,7 @@ export const SPONSORS: Array<Sponsors> = [
 	{
 		id: "g-shock",
 		name: "G-Shock",
+		icon: GShock,
 		url: "https://gshock.casio.com/es/",
 		image: {
 			width: 200 * 0.65,
