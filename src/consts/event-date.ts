d7b72e60abe333b5ce90cbc48bbff28e6d57a8c8
Polo_1245 <81937184+poloputoamo@users.noreply.github.com>
2025-03-10 21:10:00 +0100

    update: event-date.ts

--- src/consts/event-date.ts
+++ src/consts/event-date.ts
@@ -1,1 +1,11 @@
- 
+export const EVENT_TIMESTAMP = 1741631400000
+
+export const isEventExpired = (): boolean => {
+  const currentTimestamp = new Date().getTime()
+  return currentTimestamp > EVENT_TIMESTAMP
+}
+
+export const getTwitchUrl = (channel: string): string => {
+  const twitchParentUrl = import.meta.env.PROD ? "www.infolavelada.com" : "localhost"
+  return `https://player.twitch.tv/?channel=${channel}&parent=${twitchParentUrl}`
+}
