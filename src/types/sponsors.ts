5c487dcf450d3a6d350c15e4bfee31a666d071fc
midudev <miduga@gmail.com>
2025-03-18 19:50:37 +0100

    Agregar componente Sponsors en la página principal y estilos globales

---
+++ src/types/sponsors.ts
@@ -0,0 +1,36 @@
+type SponsorId =
+| "vicio"
+| "revolut"
+| "alsa"
+| "spotify"
+| "cerave"
+| "grefusa"
+| "maxibon"
+| "infojobs"
+| "coca-cola"
+| "mahou"
+| "nothing"
+
+type SponsorName = 
+| "Vicio"
+| "Revolut"
+| "Alsa"
+| "Spotify"
+| "Cerave"
+| "Grefusa"
+| "Maxibon"
+| "Infojobs"
+| "Coca-Cola"
+| "Mahou"
+| "Nothing"
+
+export interface Sponsors {
+  id: SponsorId
+  name: SponsorName
+  url: string
+  image: {
+    logo: any
+    width: number
+    height: number
+  }
+}
