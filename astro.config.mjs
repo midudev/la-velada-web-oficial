227b206cbcfea09bf311d60b9940b50ce95f82ec
midudev <miduga@gmail.com>
2025-03-18 19:49:49 +0100

    Agregar soporte para Tailwind CSS y habilitar modo sprite para SVG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  experimental: {
+    svg: {
+      mode: 'sprite',
+    }
+  }
+});
