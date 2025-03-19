01b92bd41e8a42de78427130d3a1a36e58e878e9
AGonzL <agonzlopez.11@gmail.com>
2025-03-19 08:33:07 +0100

    Actualizar configuración de Astro para incluir soporte de React y ajustar opciones de TypeScript. Se agregan dependencias para React y Tailwind CSS, y se crea un nuevo archivo de configuración de TypeScript para el entorno de Node.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    react({
+      include: ["**/*.tsx", "**/*.jsx"],
+    }),
+  ],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   experimental: {
     svg: {
-      mode: 'sprite',
-    }
-  }
+      mode: "sprite",
+    },
+  },
 });
