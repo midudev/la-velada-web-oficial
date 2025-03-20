60377a4143104f4c16fc7cb24a31b08be4095d41
Wall Monfortt <jose.gual.monfortte.f@gmail.com>
2025-03-20 15:32:49 -0600

    update Hero design

--- src/consts/fighters.ts
+++ src/consts/fighters.ts
@@ -1,58 +1,78 @@
+export interface Fighter {
+  id: string;
+  name: string;
+  opponent: string;
+}
+
 export const FIGHTERS = [
   {
     id: 'peereira',
-    name: 'Peereira'
+    name: 'Peereira',
+    opponent: 'rivaldios'
   },
   {
     id: 'perxitaa',
-    name: 'Perxitaa'
+    name: 'Perxitaa',
+    opponent: 'gaspi'
   },
   {
     id: 'abby',
-    name: 'Abby'
+    name: 'Abby',
+    opponent: 'roro'
   },
   {
     id: 'roro',
-    name: 'Roro'
+    name: 'Roro',
+    opponent: 'abby'
   },
   {
     id: 'gaspi',
-    name: 'Gaspi'
+    name: 'Gaspi',
+    opponent: 'perxitaa'
   },
   {
     id: 'rivaldios',
-    name: 'Rivaldios'
+    name: 'Rivaldios',
+    opponent: 'peereira'
   },
   {
     id: 'andoni',
-    name: 'Andoni'
+    name: 'Andoni',
+    opponent: 'carlos',
   },
   {
     id: 'viruzz',
-    name: 'Viruzz'
+    name: 'Viruzz',
+    opponent: 'tomas',
   },
   {
     id: 'alana',
-    name: 'Alana'
+    name: 'Alana',
+    opponent: 'arigeli',
   },
   {
     id: 'grefg',
-    name: 'Grefg'
+    name: 'Grefg',
+    opponent: 'westcol',
   },
   {
     id: 'westcol',
-    name: 'Westcol'
+    name: 'Westcol',
+    opponent: 'grefg',
   },
   {
     id: 'arigeli',
-    name: 'Arigeli'
+    name: 'Arigeli',
+    opponent: 'alana',
   },
   {
     id: 'tomas',
-    name: 'Tomas'
+    name: 'Tomas',
+    opponent: 'viruzz',
   },
   {
     id: 'carlos',
-    name: 'Carlos'
+    name: 'Carlos',
+    opponent: 'andoni',
   }
 ]
