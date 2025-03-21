2488f9200055e07daac5bb519ccde2513ae27073
Yeisonfjrd <andresfajardo1606@gmail.com>
2025-03-20 22:02:12 -0300

    Actualizacion: Actualize el hero y ordene el array de los luchadores

--- src/consts/fighters.ts
+++ src/consts/fighters.ts
@@ -1,58 +1,26 @@
 export const FIGHTERS = [
-  {
-    id: 'peereira',
-    name: 'Peereira'
-  },
-  {
-    id: 'perxitaa',
-    name: 'Perxitaa'
-  },
-  {
-    id: 'abby',
-    name: 'Abby'
-  },
-  {
-    id: 'roro',
-    name: 'Roro'
-  },
-  {
-    id: 'gaspi',
-    name: 'Gaspi'
-  },
-  {
-    id: 'rivaldios',
-    name: 'Rivaldios'
-  },
-  {
-    id: 'andoni',
-    name: 'Andoni'
-  },
-  {
-    id: 'viruzz',
-    name: 'Viruzz'
-  },
-  {
-    id: 'alana',
-    name: 'Alana'
-  },
-  {
-    id: 'grefg',
-    name: 'Grefg'
-  },
-  {
-    id: 'westcol',
-    name: 'Westcol'
-  },
-  {
-    id: 'arigeli',
-    name: 'Arigeli'
-  },
-  {
-    id: 'tomas',
-    name: 'Tomas'
-  },
-  {
-    id: 'carlos',
-    name: 'Carlos'
-  }
-]
+  { id: 'peereira', name: 'Peereira7' },
+  { id: 'perxitaa', name: 'Perxitaa' },
+  { id: 'abby', name: 'Abby' },
+  { id: 'roro', name: 'Roro' },
+  { id: 'gaspi', name: 'Gaspi' },
+  { id: 'rivaldios', name: 'Rivaldios' },
+  { id: 'andoni', name: 'Andoni' },
+  { id: 'viruzz', name: 'ViruZz' },
+  { id: 'alana', name: 'Alana' },
+  { id: 'grefg', name: 'TheGrefg' },
+  { id: 'westcol', name: 'WestCOL' },
+  { id: 'arigeli', name: 'Arigeli' },
+  { id: 'tomas', name: 'Tomás Mazza' },
+  { id: 'carlos', name: 'Carlos Belcast' },
+];
+
+export const MATCHUPS = [
+  { fighter1: 'peereira', fighter2: 'rivaldios' },
+  { fighter1: 'perxitaa', fighter2: 'gaspi' },
+  { fighter1: 'abby', fighter2: 'roro' },
+  { fighter1: 'andoni', fighter2: 'carlos' },
+  { fighter1: 'alana', fighter2: 'arigeli' },
+  { fighter1: 'viruzz', fighter2: 'tomas' },
+  { fighter1: 'grefg', fighter2: 'westcol' },
+];
