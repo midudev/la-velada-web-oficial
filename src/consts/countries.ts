35bee32488b7e5ae5b07579e80dbea3903242c0b
midudev <miduga@gmail.com>
2025-04-25 22:12:20 +0200

    Add fighter bios and combat descriptions, enhance country data with continents

--- src/consts/countries.ts
+++ src/consts/countries.ts
@@ -3,25 +3,36 @@ import mx from '../../public/images/flags/mx.webp'
 import co from '../../public/images/flags/co.webp'
 import es from '../../public/images/flags/es.webp'
 
-export const countries = [
+export type Country = {
+  id: string;
+  name: string;
+  image: any; // Assuming the image type is any for now
+  continent: string;
+};
+
+export const countries: Country[] = [
   {
     id: 'ar',
     name: 'Argentina',
     image: ar,
+    continent: 'South America',
   },
   {
     id: 'mx',
     name: 'México',
     image: mx,
+    continent: 'North America',
   },
   {
     id: 'co',
     name: 'Colombia',
     image: co,
+    continent: 'South America',
   },
   {
     id: 'es',
     name: 'España',
     image: es,
+    continent: 'Europe',
   },
 ]
