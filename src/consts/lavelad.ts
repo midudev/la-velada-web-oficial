1f1d957bde760ecf3581c3c3a9731b550d97ad0d
Leendy Kasandra Escamilla Cardona <62487860+Leendy@users.noreply.github.com>
2025-07-30 14:52:38 -0600

    Add files via upload

---
+++ src/consts/lavelad.ts
@@ -0,0 +1,104 @@
+import type { Photo } from '@/types/photos'
+
+export const photos: Photo[] = [
+  {
+    id: 1,
+    src: '/images/lavelada/1.webp',
+    alt: 'La Velada del Año - Foto 1'
+  },
+  {
+    id: 2,
+    src: '/images/lavelada/2.webp',
+    alt: 'La Velada del Año - Foto 2'
+  },
+  {
+    id: 3,
+    src: '/images/lavelada/3.webp',
+    alt: 'La Velada del Año - Foto 3'
+  },
+  {
+    id: 4,
+    src: '/images/lavelada/4.webp',
+    alt: 'La Velada del Año - Foto 4'
+  },
+  {
+    id: 5,
+    src: '/images/lavelada/5.webp',
+    alt: 'La Velada del Año - Foto 5'
+  },
+  {
+    id: 6,
+    src: '/images/lavelada/6.webp',
+    alt: 'La Velada del Año - Foto 6'
+  },
+  {
+    id: 7,
+    src: '/images/lavelada/7.webp',
+    alt: 'La Velada del Año - Foto 7'
+  },
+  {
+    id: 8,
+    src: '/images/lavelada/8.webp',
+    alt: 'La Velada del Año - Foto 8'
+  },
+  {
+    id: 9,
+    src: '/images/lavelada/9.webp',
+    alt: 'La Velada del Año - Foto 9'
+  },
+  {
+    id: 10,
+    src: '/images/lavelada/10.webp',
+    alt: 'La Velada del Año - Foto 10'
+  },
+  {
+    id: 11,
+    src: '/images/lavelada/11.webp',
+    alt: 'La Velada del Año - Foto 11'
+  },
+  {
+    id: 12,
+    src: '/images/lavelada/12.webp',
+    alt: 'La Velada del Año - Foto 12'
+  },
+  {
+    id: 13,
+    src: '/images/lavelada/13.webp',
+    alt: 'La Velada del Año - Foto 13 '
+  },
+  {
+    id: 14,
+    src: '/images/lavelada/14.webp',
+    alt: 'La Velada del Año - Foto 14'
+  },
+  {
+    id: 15,
+    src: '/images/lavelada/15.webp',
+    alt: 'La Velada del Año - Foto 15'
+  },
+  {
+    id: 16,
+    src: '/images/lavelada/16.webp',
+    alt: 'La Velada del Año - Foto 16'
+  },
+  {
+    id: 17,
+    src: '/images/lavelada/17.webp',
+    alt: 'La Velada del Año - Foto 17'
+  },
+  {
+    id: 18,
+    src: '/images/lavelada/18.webp',
+    alt: 'La Velada del Año - Foto 18'
+  },
+  {
+    id: 19,
+    src: '/images/lavelada/19.webp',
+    alt: 'La Velada del Año - Foto 19'
+  },
+  {
+    id: 20,
+    src: '/images/lavelada/20.webp',
+    alt: 'La Velada del Año - Foto 20'
+  }
+]
