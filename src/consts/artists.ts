f7f5ca1598a37644ba79e65144e72e01f46acd2b
Héctor Torres <hector.torres23@estudiantes.uva.es>
2025-07-30 17:24:06 +0200

    Adds URLs for artists
    Adds a URL property to each artist in the ARTISTS array.
    
    This allows linking to a specific resource (e.g., a music video)
    for each artist, enhancing the user experience.

--- src/consts/artists.ts
+++ src/consts/artists.ts
@@ -5,35 +5,42 @@ export const ARTISTS: Artist[] = [
     id: 'melendi',
     name: 'Melendi',
     image: '/images/artists/melendi.webp',
+    url: 'PlH-TDY0PC0',
   },
   {
     id: 'grupofrontera',
     name: 'Grupo Frontera',
     image: '/images/artists/grupofrontera.webp',
+    url: 'FLQkGLOXWHI',
   },
   {
     id: 'losdelrio',
     name: 'Los Del Río',
     image: '/images/artists/losdelrio.webp',
+    url: 'Zf4h98FjaD0',
   },
   {
     id: 'delarose',
     name: 'Delarose',
     image: '/images/artists/delarose.webp',
+    url: 'I9vXiS6wCWs',
   },
   {
     id: 'eladiocarrion',
     name: 'Eladio Carrion',
     image: '/images/artists/eladiocarrion.webp',
+    url: 'IYpgC6K59Nc',
   },
   {
     id: 'aitana',
     name: 'Aitana',
     image: '/images/artists/aitana.webp',
+    url: '9TrPsVfpkjg',
   },
   {
     id: 'myketowers',
     name: 'Myke Towers',
     image: '/images/artists/myketowers.webp',
+    url: 'v1GO9iRuQug',
   },
 ]
