3846b4d6a16dc0ec2f37ec011c91323c1c0a730d
Miguel Ángel Durán (midudev) <miduga@gmail.com>
2026-05-09 19:39:46 +0200

    feat: update image paths and add base URL handling across components and configuration files

--- src/utils/seoConfig.ts
+++ src/utils/seoConfig.ts
@@ -34,17 +34,17 @@ export const manifest: Partial<ManifestOptions> = {
 	display: "fullscreen",
 	icons: [
 		{
-			src: "/img/icons/favicon-192x192.png",
+			src: "img/icons/favicon-192x192.png",
 			sizes: "192x192",
 			type: "image/png",
 		},
 		{
-			src: "/img/icons/favicon-512x512.png",
+			src: "img/icons/favicon-512x512.png",
 			sizes: "512x512",
 			type: "image/png",
 		},
 		{
-			src: "/img/icons/favicon-512x512.png",
+			src: "img/icons/favicon-512x512.png",
 			sizes: "512x512",
 			type: "image/png",
 			purpose: "any maskable",
