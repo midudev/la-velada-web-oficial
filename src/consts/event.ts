29e97a12bed13fe2d84a483740665ae408afdd79
Felipe Martinez <jempeluqueria@hotmail.com>
2026-05-16 10:34:01 -0300

    feat: improve UX/UI with centralized constants, premium animations and brand effects

---
+++ src/consts/event.ts
@@ -0,0 +1,14 @@
+export const EVENT_DATE = new Date('2026-07-25T20:00:00+02:00')
+export const EVENT_TIMESTAMP = EVENT_DATE.getTime()
+export const EVENT_ISO = EVENT_DATE.toISOString()
+
+export const SITE_URL = 'https://www.infolavelada.com'
+export const TWITCH_CHANNEL_URL = 'https://www.twitch.tv/ibai'
+
+export const EVENT_LOCATION = {
+  name: 'Estadio de La Cartuja',
+  city: 'Sevilla',
+  region: 'Andalucía',
+  country: 'España',
+  countryCode: 'ES',
+}
---
+++ src/consts/site-config.ts
@@ -0,0 +1,24 @@
+export const SITE_NAME = 'La Velada del Año VI'
+export const SITE_YEAR = '2026'
+export const SITE_EDITION = 'VI'
+
+export const SOCIAL_NETWORKS = [
+  {
+    name: 'Twitch',
+    url: 'https://twitch.tv/ibai',
+    handle: '/IBAI',
+    color: '#a970ff',
+  },
+  {
+    name: 'TikTok',
+    url: 'https://tiktok.com/@ibaillanos',
+    handle: '/IBAILLANOS',
+    color: '#00f2ea',
+  },
+  {
+    name: 'YouTube',
+    url: 'https://youtube.com/@IbaiLlanos',
+    handle: '/IBAILLANOS',
+    color: '#ff0000',
+  },
+]
