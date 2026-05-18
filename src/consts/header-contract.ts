0b32e7e81ef47e5d1f4c1973a2f3a05727d41a3f
Antonio Bonet <tonybonet31@yahoo.es>
2026-05-19 04:28:08 +0700

    refactor(header): trim nav abstraction

--- src/consts/header-contract.ts
+++ src/consts/header-contract.ts
@@ -1,4 +1,3 @@
-export const HEADER_MOBILE_MENU_OPEN_ATTR = 'data-mobile-menu-open'
 export const HEADER_MOBILE_MENU_TOGGLE_EVENT = 'header:mobile-menu-toggle'
 
 export type HeaderMobileMenuToggleEvent = CustomEvent<{ open: boolean }>
