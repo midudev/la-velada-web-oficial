32723154b2c3344d2e23df0f484fe9fa31877629
JostingHer <jostingjmhernandez@gmail.com>
2026-06-12 22:36:05 +0200

    data(combat): add alondrissa face to face video

--- src/consts/battle-face-to-face-videos.ts
+++ src/consts/battle-face-to-face-videos.ts
@@ -1,6 +1,7 @@
 export type BattleFaceToFaceVideoUrl = `https://www.youtube.com/watch?${string}` | `https://youtu.be/${string}`
 
 export const battleFaceToFaceVideoUrlsById: Partial<Record<string, BattleFaceToFaceVideoUrl>> = {
+  'alondrissa-vs-angie-velasco': 'https://youtu.be/egYBxDTpx9w?si=z5yYe54PWp5qfMkT',
   'viruzz-vs-gero-arias': 'https://youtu.be/260TXvUXM_k?si=kKgDPg7Yvidl_BLU',
   'samy-rivers-vs-roro': 'https://youtu.be/9a5h7emOK6c?si=GmK0K237ZTE27Uib',
   'marta-diaz-vs-tatiana-kaer': 'https://youtu.be/kruLhBzu7Ng?si=qQ0IM-CIO6dMW-ej',
