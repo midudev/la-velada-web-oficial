7f8c14a5351f8c9fe2deb2aaa195e97c246d1e97
JostingHer <jostingjmhernandez@gmail.com>
2026-06-12 22:36:29 +0200

    data(combat): add more face to face videos

--- src/consts/battle-face-to-face-videos.ts
+++ src/consts/battle-face-to-face-videos.ts
@@ -1,6 +1,10 @@
 export type BattleFaceToFaceVideoUrl = `https://www.youtube.com/watch?${string}` | `https://youtu.be/${string}`
 
 export const battleFaceToFaceVideoUrlsById: Partial<Record<string, BattleFaceToFaceVideoUrl>> = {
+  'edu-aguirre-vs-gaston-edul': 'https://youtu.be/N1DN0w48Dzk?si=kiMPlwGFnpFcCa7I',
+  'la-parce-vs-fabiana-sevillano': 'https://youtu.be/1GRJ5nHFn48?si=HbcRAFOKDDwd9vB7',
+  'clersss-vs-natalia-mx': 'https://youtu.be/bWh2-WcdQzA?si=zQrcrXd7FKwDg9Vh',
+  'lit-killah-vs-kidd-keo': 'https://youtu.be/Ze-KnQiuwf8?si=I0Oq3o6bu8MbOTjp',
   'alondrissa-vs-angie-velasco': 'https://youtu.be/egYBxDTpx9w?si=z5yYe54PWp5qfMkT',
   'viruzz-vs-gero-arias': 'https://youtu.be/260TXvUXM_k?si=kKgDPg7Yvidl_BLU',
   'samy-rivers-vs-roro': 'https://youtu.be/9a5h7emOK6c?si=GmK0K237ZTE27Uib',
