f87f411b43e9d32315481695795d12269deab489
Leendy <leendy_escamilla@outlook.com>
2026-06-13 19:12:23 -0600

    ficha de boxeadores
    creación de las fichas e los boxeadores junto con imágenes de sus videos de entrenamiento

--- src/consts/lavelad.ts
+++ src/consts/lavelad.ts
@@ -1,104 +1,14 @@
-import type { Photo } from '@/types/photos'
-
-export const photos: Photo[] = [
+export const photos = [
   {
-    id: 1,
-    src: '/images/lavelada/1.webp',
-    alt: 'La Velada del Año - Foto 1'
+    src: '/background.webp',
+    alt: 'Foto de La Velada VI en Sevilla',
   },
   {
-    id: 2,
-    src: '/images/lavelada/2.webp',
-    alt: 'La Velada del Año - Foto 2'
+    src: '/background-1280.webp',
+    alt: 'Ambiente del evento de La Velada VI',
   },
   {
-    id: 3,
-    src: '/images/lavelada/3.webp',
-    alt: 'La Velada del Año - Foto 3'
+    src: '/background-2048.webp',
+    alt: 'Escenario nocturno de La Velada VI',
   },
-  {
-    id: 4,
-    src: '/images/lavelada/4.webp',
-    alt: 'La Velada del Año - Foto 4'
-  },
-  {
-    id: 5,
-    src: '/images/lavelada/5.webp',
-    alt: 'La Velada del Año - Foto 5'
-  },
-  {
-    id: 6,
-    src: '/images/lavelada/6.webp',
-    alt: 'La Velada del Año - Foto 6'
-  },
-  {
-    id: 7,
-    src: '/images/lavelada/7.webp',
-    alt: 'La Velada del Año - Foto 7'
-  },
-  {
-    id: 8,
-    src: '/images/lavelada/8.webp',
-    alt: 'La Velada del Año - Foto 8'
-  },
-  {
-    id: 9,
-    src: '/images/lavelada/9.webp',
-    alt: 'La Velada del Año - Foto 9'
-  },
-  {
-    id: 10,
-    src: '/images/lavelada/10.webp',
-    alt: 'La Velada del Año - Foto 10'
-  },
-  {
-    id: 11,
-    src: '/images/lavelada/11.webp',
-    alt: 'La Velada del Año - Foto 11'
-  },
-  {
-    id: 12,
-    src: '/images/lavelada/12.webp',
-    alt: 'La Velada del Año - Foto 12'
-  },
-  {
-    id: 13,
-    src: '/images/lavelada/13.webp',
-    alt: 'La Velada del Año - Foto 13 '
-  },
-  {
-    id: 14,
-    src: '/images/lavelada/14.webp',
-    alt: 'La Velada del Año - Foto 14'
-  },
-  {
-    id: 15,
-    src: '/images/lavelada/15.webp',
-    alt: 'La Velada del Año - Foto 15'
-  },
-  {
-    id: 16,
-    src: '/images/lavelada/16.webp',
-    alt: 'La Velada del Año - Foto 16'
-  },
-  {
-    id: 17,
-    src: '/images/lavelada/17.webp',
-    alt: 'La Velada del Año - Foto 17'
-  },
-  {
-    id: 18,
-    src: '/images/lavelada/18.webp',
-    alt: 'La Velada del Año - Foto 18'
-  },
-  {
-    id: 19,
-    src: '/images/lavelada/19.webp',
-    alt: 'La Velada del Año - Foto 19'
-  },
-  {
-    id: 20,
-    src: '/images/lavelada/20.webp',
-    alt: 'La Velada del Año - Foto 20'
-  }
 ]
