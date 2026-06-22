a0a4895b7853b5b6cfd4504da3b79d9a5210fd8e
madeval <alvarez.fing@gmail.com>
2026-06-22 15:55:25 -0300

    chore: update image sources to use CDN links for various assets

--- src/consts/lavelad.ts
+++ src/consts/lavelad.ts
@@ -1,14 +1,14 @@
 export const photos = [
   {
-    src: '/background.webp',
+    src: 'https://cdn.infolavelada.com/hero-bg/background.webp',
     alt: 'Foto de La Velada VI en Sevilla',
   },
   {
-    src: '/background-1280.webp',
+    src: 'https://cdn.infolavelada.com/hero-bg/background-1280.webp',
     alt: 'Ambiente del evento de La Velada VI',
   },
   {
-    src: '/background-2048.webp',
+    src: 'https://cdn.infolavelada.com/hero-bg/background-2048.webp',
     alt: 'Escenario nocturno de La Velada VI',
   },
 ]
